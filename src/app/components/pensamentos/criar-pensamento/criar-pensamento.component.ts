88686028b6a3bab6549be8386a35b81e73b7ec9c
Luiz Aguiar <luiz.aguiar@totvs.com.br>
2023-05-25 23:07:44 -0300

    initial commit

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css'],
+})
+export class CriarPensamentoComponent {
+  constructor(private service: PensamentoService, private router: Router) {}
+
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1',
+  };
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
+    });
+  }
+  cancelar() {
+    this.router.navigate(['/listarPensamento']);
+  }
+}
---
+++ src/app/components/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css'],
+})
+export class ExcluirPensamentoComponent {
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: '',
+  };
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      console.log(pensamento);
+      this.pensamento = pensamento;
+    });
+    console.log(id);
+  }
+
+  excluirPensamento() {
+    if (this.pensamento.id) {
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      });
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento']);
+  }
+}
